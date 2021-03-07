81512cb72fdbbb016bba08264effcf54640aa35d
Jon Jagger <jon@jaggersoft.com>
2021-03-07 21:45:01 +0000

    Try exporting

--- start_point/hiker_test.go
+++ start_point/hiker_test.go
@@ -2,7 +2,7 @@ package hiker
 
 import (
     "testing"
-    "github.com/stretchr/testify/assert"
+    _ "github.com/stretchr/testify/assert"
 )
 
 func Test_life_the_universe_and_everything(t *testing.T) {
